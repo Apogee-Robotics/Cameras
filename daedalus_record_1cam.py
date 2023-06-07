f9bfd173e484fd421ec7b960d463a44e56157e57
conruybalid <conruybalid@gmail.com>
2023-06-07 02:43:09 +0100

    Switched to 720p recording at 21 FPS

--- daedalus_record_1cam.py
+++ daedalus_record_1cam.py
@@ -4,42 +4,54 @@ import time
 import os
 
 
-SHOW_FRAME = 0
-NUM_FILES = 5
-NUM_FRAMES_PER_FILE = 150
-WIDTH = 640
-HEIGHT = 480
-FPS = 30
+SHOW_FRAME = False
+TIME_TO_RECORD = 5
+FPS = 21
+NUM_FRAMES_PER_FILE = TIME_TO_RECORD * FPS
+WIDTH = 1280
+HEIGHT = 720
+
 
 
 
 print(f'Using OpenCV version {cv2.__version__}')
 
-date = time.strftime("%d-%b-%Y", time.localtime())
+#time.sleep(10)
+
+testNumFile = open("/home/pi/TestVideos/testNumber.txt", 'r')
+testNum = int(testNumFile.read())
+testNumFile.close()
+
+#date = time.strftime("%d-%b-%Y", time.localtime())
 #if gp.input(31):
-os.system(f'mkdir /home/pi/TestVideos/{date}')
-file_folder = f'/home/pi/TestVideos/{date}'
+os.system(f'mkdir /home/pi/TestVideos/test{testNum}')
+file_folder = f'/home/pi/TestVideos/test{testNum}'
+testNumFile = open("/home/pi/TestVideos/testNumber.txt", 'w')
+testNumFile.write(f'{testNum + 1}')
+testNumFile.close()
 #else:
 #    file_folder = f'/home/pi/OfficialVideo/{date}'
 
 log = open(f'{file_folder}/log.txt', 'a')
 
 hms = time.strftime('%H-%M-%S', time.localtime())
-log.write(f'{hms}\tPi was turned on\n')
+log.write(f'\tPi was turned on\n')
+
 
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH,WIDTH)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT,HEIGHT)
 capture.set(cv2.CAP_PROP_FPS,FPS)
 
+
 #Test Camera
 if(not capture.isOpened()):
-    hms = time.strftime('%H-%M-%S', time.localtime())
-    log.write(f'{hms}\tCamera is not open\n')
-    print(f'Camera is not open')
+    #hms = time.strftime('%H-%M-%S', time.localtime())
+    log.write(f'\tCamera is NOT open\n')
+    print(f'Camera is NOT open')
 else:
-    hms = time.strftime('%H-%M-%S', time.localtime())
-    log.write(f'{hms}\tCamera is open\n')
+    #hms = time.strftime('%H-%M-%S', time.localtime())
+    log.write(f'\tCamera is open\n')
     print(f'Camera is open')
 
 fcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -49,11 +61,13 @@ gp.setup(37,gp.IN)
 gp.setup(31,gp.IN)
 
 
-hms = time.strftime('%H-%M-%S', time.localtime())
-log.write(f'{hms}\tWaiting for start...\n')
+#hms = time.strftime('%H-%M-%S', time.localtime())
+log.write(f'\tWaiting for start...\n')
 print('Waiting for start...')
 while(not gp.input(37)):
-   time.sleep(1)
+    while(not gp.input(37)):
+        time.sleep(0.5)
+    time.sleep(0.5)
 
 hms = time.strftime('%H-%M-%S', time.localtime())
 log.write(f'{hms}\tStarting Recording...\n')
@@ -66,7 +80,7 @@ while(gp.input(37)):
     writer = cv2.VideoWriter(file_name, fcc, FPS, (WIDTH, HEIGHT))
 
     for frame_num in range(NUM_FRAMES_PER_FILE): 
-         
+        
         rt, frame = capture.read()
         hms = time.strftime('%H-%M-%S', time.localtime())
         if not rt:
@@ -81,6 +95,7 @@ while(gp.input(37)):
         
         if SHOW_FRAME:
             cv2.imshow('frame', frame)
+            print('showing frame')
 
     hms = time.strftime('%H-%M-%S', time.localtime())
     log.write(f'{hms}\trelease VideoFile{file_num}\n')     
@@ -91,6 +106,7 @@ while(gp.input(37)):
 
 hms = time.strftime('%H-%M-%S', time.localtime())
 log.write(f'{hms}\tFinished Capturing\n')
+log.close()
 print('Finished Capturing')
 
 capture.release()
